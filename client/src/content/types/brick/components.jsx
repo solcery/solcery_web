9cd8168fb22c6fca9c6b619e96809623a496578b
teuzet <teuzet92@gmail.com>
2022-05-23 23:08:40 +0200

    merged bricks and custom bricks

--- client/src/content/types/brick/components.jsx
+++ client/src/content/types/brick/components.jsx
@@ -1,11 +1,11 @@
 import { ReactFlowProvider } from 'react-flow-renderer';
-import { BrickEditor } from '../editor/BrickEditor';
+import { BrickEditor } from './editor/BrickEditor';
 import React, { useState, useEffect } from "react";
-import { BrickLibrary, paramFromMapEntry } from "../../../brickLib";
+import { BrickLibrary, paramFromMapEntry } from "../../brickLib";
 import { Select } from 'antd';
-import { SType } from '../../base';
-import { insertTable } from '../../../../utils';
-import { useBrickLibrary } from '../../../../contexts/brickLibrary';
+import { SType } from '../base';
+import { insertTable } from '../../../utils';
+import { useBrickLibrary } from '../../../contexts/brickLibrary';
 
 const { Option } = Select;
 
@@ -38,49 +38,50 @@ const paramMapType = SType.from({
 })
 
 export const ValueRender = (props) => {
-  const [ brickType, setBrickType ] = useState(props.defaultValue && props.defaultValue.brickType);
-  const [ brickParams, setBrickParams ] = useState(props.defaultValue ? props.defaultValue.brickParams : []);
+  const [ brickType, setBrickType ] = useState(props.type.brickType ? props.type.brickType : props.defaultValue && props.defaultValue.brickType);
+  const [ brickParams, setBrickParams ] = useState((props.type.params && props.defaultValue) ? props.defaultValue.brickParams : []);
   const [ brickTree, setBrickTree ] = useState(props.defaultValue && props.defaultValue.brickTree);
   const { brickLibrary } = useBrickLibrary();
-  const [ brickLib, setBrickLib ] = useState(undefined)
+  const [ brickLib, setBrickLib ] = useState(undefined);
 
-  const onChange = (newValue) => {
-    if (props.onChange) props.onChange(newValue);
+  const onChange = (brickTree) => {
+    setBrickTree(brickTree)
+    if (props.onChange) props.onChange({ brickType, brickParams, brickTree });
   }
 
   useEffect(() => {
-    onChange({ brickType, brickTree, brickParams })
-  }, [ brickParams, brickType, brickTree ])
-
-  useEffect(() => {
     if (!brickLibrary) return;
-    console.log(brickLibrary);
     let bricks = {}
     for (let [ libname, funcs ] of Object.entries(brickLibrary)) {
       bricks[libname] = Object.assign({}, funcs)
     }
-    let params = brickParams.filter(entry => entry.key !== '').map(entry => paramFromMapEntry(entry));
-    params.forEach(param => {
-      let arg = argFromParam(param);
-      let argCode = arg.params[0].value;
-      insertTable(bricks, arg, arg.lib, `arg.${argCode}`);  
-    })
+    if (props.type.params) {
+      let params = brickParams.filter(entry => entry.key !== '').map(entry => paramFromMapEntry(entry));
+      params.forEach(param => {
+        let arg = argFromParam(param);
+        let argCode = arg.params[0].value;
+        insertTable(bricks, arg, arg.lib, `arg.${argCode}`);  
+      })
+    }
     setBrickLib(bricks)
   }, [ brickParams, brickLibrary ])
 
   if (!brickLibrary) return <p>Loading</p>;
   if (!brickLib) return <p>Loading</p>;
-  if (!props.onChange && !props.defaultValue) return <p>Empty</p>  
+  if (!props.onChange && !props.defaultValue.brickTree) return <p>Empty</p>  
   if (!brickType) return <BrickTypeSelector onChange = { setBrickType }/>;
 
+  
   return (
     <>
-      <p>Params:</p>
-      <paramMapType.valueRender 
-        defaultValue = { brickParams }
-        type = { paramMapType }
-        onChange = { props.onChange && setBrickParams }
-      />
+      {props.type.params && <>
+        <p>Params:</p>
+        <paramMapType.valueRender 
+          defaultValue = { brickParams }
+          type = { paramMapType }
+          onChange = { props.onChange && setBrickParams }
+        />
+      </>}
       <ReactFlowProvider>
         <BrickEditor
           width = { 300 }
@@ -89,9 +90,9 @@ export const ValueRender = (props) => {
           brickTree = { brickTree }
           brickType = { brickType }
           type = { props.type }
-          onChange = { setBrickTree }
+          onChange = { props.onChange && onChange }
         />
       </ReactFlowProvider> 
     </>
-	);
+  );
 }
--- client/src/content/types/brick/index.jsx
+++ client/src/content/types/brick/index.jsx
@@ -1,2 +1,46 @@
-export * from './brick';
-export * from './customBrick';
+import { SType } from '../../index';
+import { ValueRender } from './components';
+
+const constructBrickType = (stringType) => {
+  switch (stringType) {
+    case 'action': return 0;
+    case 'condition': return 1;
+    case 'value': return 2;
+    default: 
+      throw new Error('Error constructing brick!');
+  }
+}
+
+class SBrick {
+  static fromString = data =>  new SBrick({ brickType: data });
+
+  constructor(data) {
+    this.brickType = data.brickType;
+    this.params = data.params;
+  }
+
+  construct = (value, meta) => {
+    // let brickSignature = BrickLib.getBrick(value.type, value.func);
+    // if (!brickSignature) throw new Error("Error constructing brick!");
+    // return {
+    //   name: brickSignature.name,
+    //   type: constructBrickType(value.type),
+    //   subtype: 100, // TODO:
+    //   params: brickSignature.params
+    //     .filter(paramSig => value.params[paramSig.code])
+    //     .map(paramSig => {
+    //       let param = value.params[paramSig.code]
+    //       return {
+    //         name: paramSig.code,
+    //         value: paramSig.type.construct(param, meta)
+    //       }
+    //     })
+    // }
+  }
+  valueRender = ValueRender;
+  default = '';
+};
+
+SType.register('SBrick', SBrick);
+export { SBrick }
+
