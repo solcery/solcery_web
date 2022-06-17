d47fb2b9c896b3cf31123a8dadbe3c9214f86d06
teuzet <teuzet92@gmail.com>
2022-06-17 18:01:10 +0200

    proper filter clear behaviour

--- client/src/content/types/base/components.jsx
+++ client/src/content/types/base/components.jsx
@@ -1,18 +1,28 @@
-import { useState } from 'react';
+import React, { useState, useRef, useEffect, useCallback } from 'react';
 import { Button } from 'antd';
 
-export const DefaultFilterRender = ({ defaultValue, onChange, type }) => {
-	const [value, setValue] = useState(defaultValue);
+export const DefaultFilterRender = (props) => {
+	const [value, setValue] = useState(props.defaultValue);
+
+	const apply = useCallback(() => {
+		props.onChange(value);
+	}, [ value ])
+
+	const clear = () => {
+		props.onChange(undefined);
+	}
+
 	return (
 		<div>
-			<type.valueRender
-				type={type}
-				defaultValue={defaultValue}
+			<props.type.valueRender
+				type={props.type}
+				defaultValue={props.defaultValue}
 				onChange={setValue}
-				onPressEnter={() => onChange(value)}
+				onPressEnter={apply}
+				isFilter = { true }
 			/>
-			<Button onClick={() => onChange(value)}>APPLY</Button>
-			<Button onClick={() => onChange(undefined)}>CLEAR</Button>
+			<Button onClick={apply}>APPLY</Button>
+			<Button onClick={clear}>CLEAR</Button>
 		</div>
 	);
 };
