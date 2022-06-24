986bc9b6aa05ea4731b7169ad387fa8a43f93850
teuzet <teuzet92@gmail.com>
2022-06-24 15:59:10 +0200

    string macro link construction fix

--- client/src/content/types/string/index.jsx
+++ client/src/content/types/string/index.jsx
@@ -17,7 +17,10 @@ class SString {
 
 		// Applying macros
 		function applyLinkMacro(match, template, code) {
-			let obj = meta.rawContent[template].objects.find((obj) => obj.fields.code === code);
+			console.log(meta.rawContent)
+			let templateObjects = meta.rawContent[template];
+			if (!templateObjects) return match;
+			let obj = templateObjects.objects.find((obj) => obj.fields.code === code);
 			let intId = meta.getIntId(obj._id);
 			return `<link="${intId}">`;
 		}
