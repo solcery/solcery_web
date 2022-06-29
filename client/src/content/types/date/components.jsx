9648bacc640e2470692c0eecb09d3a7ce3fc7320
teuzet <teuzet92@gmail.com>
2022-06-29 02:39:33 +0200

    date type now is stored as timestamp

--- client/src/content/types/date/components.jsx
+++ client/src/content/types/date/components.jsx
@@ -4,13 +4,18 @@ import moment from 'moment';
 
 export const ValueRender = (props) => {
 	let format = props.type.excludeTime ? 'MMMM Do YYYY' : 'MMMM Do YYYY HH:mm';
-	let defaultValue = props.defaultValue && moment(props.defaultValue)
-	if (!props.onChange) return <p>{defaultValue && defaultValue.format(format)}</p>;
+	let defaultValue = props.defaultValue && moment.unix(props.defaultValue)
+	if (!props.onChange) return <>{defaultValue && defaultValue.format(format)}</>;
+
+	const onChange = (m) => {
+		props.onChange && props.onChange(m.unix());
+	}
+
 	return <DatePicker 
 		format = {format}
 		showTime={!props.type.excludeTime && { format: 'HH:mm' }}
 		defaultValue={defaultValue} 
-		onChange={props.onChange}
+		onChange={onChange}
 		width = {400}
 	/>;
 };
