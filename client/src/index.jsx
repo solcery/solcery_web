99e0beea51ae95cfb926d8dfd18dd62509e2f29e
teuzet <teuzet92@gmail.com>
2022-07-04 18:54:09 +0200

    empty default build

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,4 @@
 import React, { render } from 'react-dom';
-import Sage from './builds/Sage';
 
 const rootElement = document.getElementById('root');
-render(<Sage />, rootElement);
+render(<>Empty build</>, rootElement);
