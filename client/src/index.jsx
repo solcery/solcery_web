9e57a24de76eff2f6bd9712b3adcaa650e91d359
teuzet <teuzet92@gmail.com>
2022-07-23 16:37:57 +0200

    basic empty build

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,16 @@
 import React, { render } from 'react-dom';
 
-// Sage
-import Sage from './builds/Sage'
-const rootElement = document.getElementById('root');
-render(<Sage/>, rootElement);
-
-
 // Basic
+const rootElement = document.getElementById('root');
+render(<>EMPTY</>, rootElement);
+
+// Sage
+// import Sage from './builds/Sage'
 // const rootElement = document.getElementById('root');
-// render(<>EMPTY</>, rootElement);
+// render(<Sage/>, rootElement);
+
+// Game
+// import Sage from './builds/Game'
+// const rootElement = document.getElementById('root');
+// render(<Game/>, rootElement);
+
