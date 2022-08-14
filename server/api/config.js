c14d90be32579e54552241f5e538036354695323
teuzet <teuzet92@gmail.com>
2022-08-14 03:57:43 +0200

    game server config

--- server/api/config.js
+++ server/api/config.js
@@ -1,5 +1,15 @@
 const apiConfig = {
-
+	modules: [
+		{
+			name: 'project',
+			path: './project',
+		},
+		{
+			name: 'template',
+			path: './template',
+		}
+	],
+	auth: './player/auth',
 }
 
 module.exports = apiConfig;
