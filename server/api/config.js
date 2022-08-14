91cd21b8a525c660bb8ff59d7bd2339b879e071d
teuzet <teuzet92@gmail.com>
2022-08-14 03:57:00 +0200

    sage server config

--- server/api/config.js
+++ server/api/config.js
@@ -1,5 +1,19 @@
 const apiConfig = {
-
+	modules: [
+		{
+			name: 'project',
+			path: './project',
+		},
+		{
+			name: 'user',
+			path: './user',
+		},
+		{
+			name: 'template',
+			path: './template',
+		}
+	],
+	auth: './user/auth',
 }
 
 module.exports = apiConfig;
