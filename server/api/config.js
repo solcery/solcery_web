86c00b1657e738a553d1ba39a6805de138b83c22
teuzet <teuzet92@gmail.com>
2022-08-14 03:55:56 +0200

    empty server config

--- server/api/config.js
+++ server/api/config.js
@@ -1,15 +1,5 @@
 const apiConfig = {
-	modules: [
-		{
-			name: 'template',
-			path: './template',
-		},
-		{
-			name: 'game',
-			path: './game',
-		},
-	],
-	auth: './player/auth',
+
 }
 
 module.exports = apiConfig;
