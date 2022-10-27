c2430c79c8a6ff71b059358ec7d5e1de5b902a45
teuzet <teuzet92@gmail.com>
2022-10-27 03:19:30 +0200

    empty basic build

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,9 @@
 import { createRoot } from "react-dom/client";
 
-// import Sage from './builds/Sage';
-import Game from './builds/Game';
-
-// const Empty = () => <>Empty</>;
+const Empty = () => <>Empty</>;
 
 let container = document.getElementById('root')
 const root = createRoot(container);
 
-// root.render(<Sage/>);
-root.render(<Game/>);
+root.render(<>Empty</>);
 
