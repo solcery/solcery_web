0aa8df88e2b77bebcc4dc1917c46431a67c6ee90
teuzet <teuzet92@gmail.com>
2022-11-08 12:01:11 +0100

    unity builder content for local player

--- src/components/unityBuilder.jsx
+++ src/components/unityBuilder.jsx
@@ -64,11 +64,10 @@ export function UnityBuilder() {
 		session.gameState.start(session.players);
 		let unityPackage = session.gameState.exportPackage();
 		unityPackage.predict = true;
-		console.log(unityPackage)
 		setResult([
 			{
 				filename: 'game_content',
-				data: content.unity_local,
+				data: session.getUnityContent(),
 			},
 			{
 				filename: 'game_content_overrides',
